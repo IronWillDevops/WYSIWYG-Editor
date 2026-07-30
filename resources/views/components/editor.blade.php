04be9e3abca67ba25a9ce17dc4832337d251d6b6
InkForge Dev <dev@inkforge.io>
2026-07-30 15:08:29 +0300

    fix: resolve 5 Blade-component issues (props, @once, route, i18n)

--- resources/views/components/editor.blade.php
+++ resources/views/components/editor.blade.php
@@ -3,24 +3,42 @@
     'id' => null,
     'value' => '',
     'theme' => config('inkforge-editor.theme', 'auto'),
-    'locale' => config('inkforge-editor.locale', 'en'),
+    'locale' => config('inkforge-editor.locale', app()->getLocale()),
     'toolbar' => null,
     'height' => 420,
     'autosave' => null,
+    'disabledPlugins' => null,
+    'sanitizer' => null,
+    'history' => null,
+    'uploadUrl' => null,
     'options' => [],
 ])
 
 @php
-    $elementId = $id ?? $name.'-'.uniqid();
+    $elementId = $id ?? $name . '-' . uniqid();
+
+    $supportedLocales = ['en', 'uk', 'ru'];
+    if (!in_array($locale, $supportedLocales)) {
+        $locale = config('inkforge-editor.locale', 'en');
+    }
+
+    $uploadUrl = $uploadUrl ?? (app('router')->has('inkforge-editor.upload.image')
+        ? route('inkforge-editor.upload.image')
+        : null);
+
     $mergedOptions = \InkForge\Editor\Facades\InkForgeEditor::buildOptions(array_filter([
         'theme' => $theme,
         'locale' => $locale,
         'toolbar' => $toolbar,
         'height' => $height,
         'autosave' => $autosave ? array_merge(config('inkforge-editor.autosave', []), ['enabled' => true]) : null,
+        'disabledPlugins' => $disabledPlugins,
+        'sanitizer' => $sanitizer,
+        'history' => $history,
+        'uploadUrl' => $uploadUrl,
     ], static fn ($v) => $v !== null));
+
     $mergedOptions = array_replace_recursive($mergedOptions, $options);
-    $uploadUrl = route('inkforge-editor.upload.image');
 @endphp
 
 <div {{ $attributes->only('class') }} data-inkforge-editor-wrapper>
@@ -35,11 +53,22 @@
     <link rel="stylesheet" href="{{ asset('vendor/inkforge-editor/css/inkforge-editor.css') }}">
 @endonce
 
-<script type="module">
-    import Editor from "{{ asset('vendor/inkforge-editor/js/inkforge-editor.esm.js') }}";
+@once('inkforge-editor-module')
+    <script>
+        window.__inkforgeQueue = [];
+    </script>
+    <script type="module">
+        import Editor from "{{ asset('vendor/inkforge-editor/js/inkforge-editor.esm.js') }}";
+        (window.__inkforgeQueue || []).forEach(function (args) {
+            Editor.init(args[0], args[1]);
+        });
+        window.InkForgeEditor = Editor;
+    </script>
+@endonce
 
-    Editor.init('#{{ $elementId }}', Object.assign(
-        { uploadUrl: @json($uploadUrl) },
-        @json($mergedOptions)
-    ));
+<script>
+    window.__inkforgeQueue.push([
+        @json('#' . $elementId),
+        {!! json_encode($mergedOptions) !!}
+    ]);
 </script>
