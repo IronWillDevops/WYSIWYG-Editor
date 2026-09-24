IronWillDevops/WYSIWYG-Editor

commit 4361c51057c4aeb932364187f75e41e60ca751f9

fix: use raw output for textarea value in editor blade template

resources/views/components/editor.blade.php
@@ -46,7 +46,7 @@
         name="{{ $name }}"
         id="{{ $elementId }}"
         style="display:none"
-    >{{ $value }}</textarea>
+    >{!! $value !!}</textarea>
 </div>
 
 @once

resources/js/dist/FindModule-B_JBBIJf.js.map
@@ -1,1 +1,0 @@
-{"version":3,"file":"FindModule-B_JBBIJf.js","sources":["../src/modules/FindModule.js"],"sourcesContent":["import Dialog from '../utils/Dialog.js';\n\n/**\n * Find & Replace across the editor's text content, with optional\n * regular-expression and case-sensitive matching, plus \"highlight all\".\n */\nexport default class FindModule {\n    constructor(editor) {\n        this.editor = editor;\n        this.matches = [];\n        this.currentIndex = -1;\n    }\n\n    open() {\n        this.clearHighlights();\n\n        const body = `\n            <label class=\"ife-field\">\n                <span>Find</span>\n                <input type=\"text\" name=\"query\" required autofocus>\n            </label>\n            <label class=\"ife-field\">\n                <span>Replace with</span>\n                <input type=\"text\" name=\"replacement\">\n            </label>\n            <label class=\"ife-field--inline\"><input type=\"checkbox\" name=\"caseSensitive\"> Case sensitive</label>\n            <label class=\"ife-field--inline\"><input type=\"checkbox\" name=\"useRegex\"> Regular expression</label>\n        `;\n\n        this.dialog = new Dialog(this.editor.wrapper, {\n            title: 'Find & Replace',\n            bodyHtml: body,\n            confirmLabel: 'Replace all',\n            onConfirm: (form) => this.replaceAll(form),\n            onClose: () => this.clearHighlights(),\n        });\n\n        const findNextBtn = document.createElement('button');\n        findNextBtn.type = 'button';\n        findNextBtn.className = 'ife-btn ife-btn--ghost';\n        findNextBtn.textContent = 'Highlight all';\n        findNextBtn.addEventListener('mousedown', (e) => e.preventDefault());\n        findNextBtn.addEventListener('click', (e) => {\n            e.stopPropagation();\n            this.highlightAll(new FormData(this.dialog.form));\n        });\n\n        this.dialog.open();\n        this.dialog.form.querySelector('.ife-dialog__footer').prepend(findNextBtn);\n    }\n\n    buildRegex(form) {\n        const query = String(form.get('query') ?? '').trim();\n        if (!query) return null;\n        const caseSensitive = Boolean(form.get('caseSensitive'));\n        const useRegex = Boolean(form.get('useRegex'));\n        const flags = `g${caseSensitive ? '' : 'i'}`;\n        const pattern = useRegex ? query : query.replace(/[.*+?^${}()|[\\]\\\\]/g, '\\\\$&');\n        return new RegExp(pattern, flags);\n    }\n\n    highlightAll(form) {\n        this.clearHighlights();\n        const query = String(form.get('query') ?? '');\n        if (!query) return;\n        const regex = this.buildRegex(form);\n        if (!regex) return;\n\n        const walker = document.createTreeWalker(this.editor.root, NodeFilter.SHOW_TEXT, null);\n        const textNodes = [];\n        let node = walker.nextNode();\n        while (node) {\n            textNodes.push(node);\n            node = walker.nextNode();\n        }\n\n        textNodes.forEach((textNode) => {\n            const text = textNode.textContent ?? '';\n            if (!regex.test(text)) return;\n            regex.lastIndex = 0;\n\n            const fragment = document.createDocumentFragment();\n            let lastIndex = 0;\n            let match = regex.exec(text);\n            while (match) {\n                fragment.appendChild(document.createTextNode(text.slice(lastIndex, match.index)));\n                const mark = document.createElement('mark');\n                mark.className = 'ife-search-highlight';\n                mark.textContent = match[0];\n                fragment.appendChild(mark);\n                lastIndex = match.index + match[0].length;\n                match = regex.exec(text);\n            }\n            fragment.appendChild(document.createTextNode(text.slice(lastIndex)));\n            textNode.replaceWith(fragment);\n        });\n    }\n\n    clearHighlights() {\n        this.editor.root.querySelectorAll('mark.ife-search-highlight').forEach((mark) => {\n            mark.replaceWith(document.createTextNode(mark.textContent ?? ''));\n        });\n        this.editor.root.normalize();\n    }\n\n    replaceAll(form) {\n        const data = new FormData(form);\n        const regex = this.buildRegex(data);\n        if (!regex) return;\n        const replacement = String(data.get('replacement') ?? '');\n\n        this.editor.history.push();\n        this.clearHighlights();\n\n        const walker = document.createTreeWalker(this.editor.root, NodeFilter.SHOW_TEXT, null);\n        const textNodes = [];\n        let node = walker.nextNode();\n        while (node) {\n            textNodes.push(node);\n            node = walker.nextNode();\n        }\n\n        textNodes.forEach((textNode) => {\n            textNode.textContent = (textNode.textContent ?? '').replace(regex, replacement);\n        });\n\n        this.editor.emitChange();\n    }\n\n    destroy() {\n        this.clearHighlights();\n        this.dialog?.close();\n    }\n}\n"],"names":["FindModule","editor","body","Dialog","form","findNextBtn","e","query","caseSensitive","useRegex","flags","pattern","regex","walker","textNodes","node","textNode","text","fragment","lastIndex","match","mark","data","replacement","_a"],"mappings":";AAMe,MAAMA,EAAW;AAAA,EAC5B,YAAYC,GAAQ;AAChB,SAAK,SAASA,GACd,KAAK,UAAU,CAAA,GACf,KAAK,eAAe;AAAA,EACxB;AAAA,EAEA,OAAO;AACH,SAAK,gBAAe;AAEpB,UAAMC,IAAO;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAab,SAAK,SAAS,IAAIC,EAAO,KAAK,OAAO,SAAS;AAAA,MAC1C,OAAO;AAAA,MACP,UAAUD;AAAA,MACV,cAAc;AAAA,MACd,WAAW,CAACE,MAAS,KAAK,WAAWA,CAAI;AAAA,MACzC,SAAS,MAAM,KAAK,gBAAe;AAAA,IAC/C,CAAS;AAED,UAAMC,IAAc,SAAS,cAAc,QAAQ;AACnD,IAAAA,EAAY,OAAO,UACnBA,EAAY,YAAY,0BACxBA,EAAY,cAAc,iBAC1BA,EAAY,iBAAiB,aAAa,CAACC,MAAMA,EAAE,gBAAgB,GACnED,EAAY,iBAAiB,SAAS,CAACC,MAAM;AACzC,MAAAA,EAAE,gBAAe,GACjB,KAAK,aAAa,IAAI,SAAS,KAAK,OAAO,IAAI,CAAC;AAAA,IACpD,CAAC,GAED,KAAK,OAAO,KAAI,GAChB,KAAK,OAAO,KAAK,cAAc,qBAAqB,EAAE,QAAQD,CAAW;AAAA,EAC7E;AAAA,EAEA,WAAWD,GAAM;AACb,UAAMG,IAAQ,OAAOH,EAAK,IAAI,OAAO,KAAK,EAAE,EAAE,KAAI;AAClD,QAAI,CAACG,EAAO,QAAO;AACnB,UAAMC,IAAgB,EAAQJ,EAAK,IAAI,eAAe,GAChDK,IAAW,EAAQL,EAAK,IAAI,UAAU,GACtCM,IAAQ,IAAIF,IAAgB,KAAK,GAAG,IACpCG,IAAUF,IAAWF,IAAQA,EAAM,QAAQ,uBAAuB,MAAM;AAC9E,WAAO,IAAI,OAAOI,GAASD,CAAK;AAAA,EACpC;AAAA,EAEA,aAAaN,GAAM;AAGf,QAFA,KAAK,gBAAe,GAEhB,CADU,OAAOA,EAAK,IAAI,OAAO,KAAK,EAAE,EAChC;AACZ,UAAMQ,IAAQ,KAAK,WAAWR,CAAI;AAClC,QAAI,CAACQ,EAAO;AAEZ,UAAMC,IAAS,SAAS,iBAAiB,KAAK,OAAO,MAAM,WAAW,WAAW,IAAI,GAC/EC,IAAY,CAAA;AAClB,QAAIC,IAAOF,EAAO,SAAQ;AAC1B,WAAOE;AACH,MAAAD,EAAU,KAAKC,CAAI,GACnBA,IAAOF,EAAO,SAAQ;AAG1B,IAAAC,EAAU,QAAQ,CAACE,MAAa;AAC5B,YAAMC,IAAOD,EAAS,eAAe;AACrC,UAAI,CAACJ,EAAM,KAAKK,CAAI,EAAG;AACvB,MAAAL,EAAM,YAAY;AAElB,YAAMM,IAAW,SAAS,uBAAsB;AAChD,UAAIC,IAAY,GACZC,IAAQR,EAAM,KAAKK,CAAI;AAC3B,aAAOG,KAAO;AACV,QAAAF,EAAS,YAAY,SAAS,eAAeD,EAAK,MAAME,GAAWC,EAAM,KAAK,CAAC,CAAC;AAChF,cAAMC,IAAO,SAAS,cAAc,MAAM;AAC1C,QAAAA,EAAK,YAAY,wBACjBA,EAAK,cAAcD,EAAM,CAAC,GAC1BF,EAAS,YAAYG,CAAI,GACzBF,IAAYC,EAAM,QAAQA,EAAM,CAAC,EAAE,QACnCA,IAAQR,EAAM,KAAKK,CAAI;AAAA,MAC3B;AACA,MAAAC,EAAS,YAAY,SAAS,eAAeD,EAAK,MAAME,CAAS,CAAC,CAAC,GACnEH,EAAS,YAAYE,CAAQ;AAAA,IACjC,CAAC;AAAA,EACL;AAAA,EAEA,kBAAkB;AACd,SAAK,OAAO,KAAK,iBAAiB,2BAA2B,EAAE,QAAQ,CAACG,MAAS;AAC7E,MAAAA,EAAK,YAAY,SAAS,eAAeA,EAAK,eAAe,EAAE,CAAC;AAAA,IACpE,CAAC,GACD,KAAK,OAAO,KAAK,UAAS;AAAA,EAC9B;AAAA,EAEA,WAAWjB,GAAM;AACb,UAAMkB,IAAO,IAAI,SAASlB,CAAI,GACxBQ,IAAQ,KAAK,WAAWU,CAAI;AAClC,QAAI,CAACV,EAAO;AACZ,UAAMW,IAAc,OAAOD,EAAK,IAAI,aAAa,KAAK,EAAE;AAExD,SAAK,OAAO,QAAQ,KAAI,GACxB,KAAK,gBAAe;AAEpB,UAAMT,IAAS,SAAS,iBAAiB,KAAK,OAAO,MAAM,WAAW,WAAW,IAAI,GAC/EC,IAAY,CAAA;AAClB,QAAIC,IAAOF,EAAO,SAAQ;AAC1B,WAAOE;AACH,MAAAD,EAAU,KAAKC,CAAI,GACnBA,IAAOF,EAAO,SAAQ;AAG1B,IAAAC,EAAU,QAAQ,CAACE,MAAa;AAC5B,MAAAA,EAAS,eAAeA,EAAS,eAAe,IAAI,QAAQJ,GAAOW,CAAW;AAAA,IAClF,CAAC,GAED,KAAK,OAAO,WAAU;AAAA,EAC1B;AAAA,EAEA,UAAU;;AACN,SAAK,gBAAe,IACpBC,IAAA,KAAK,WAAL,QAAAA,EAAa;AAAA,EACjB;AACJ;"}

resources/js/dist/FindModule-BzDbrX7G.js
@@ -1,4 +1,4 @@
-import { D as g } from "./index-Ccv0d7hY.js";
+import { D as g } from "./index-DdpIAR1K.js";
 class x {
   constructor(e) {
     this.editor = e, this.matches = [], this.currentIndex = -1;
@@ -83,4 +83,4 @@ class x {
 export {
   x as default
 };
-//# sourceMappingURL=FindModule-B_JBBIJf.js.map
+//# sourceMappingURL=FindModule-BzDbrX7G.js.map

resources/js/dist/FindModule-BzDbrX7G.js.map
@@ -1,0 +1,1 @@
+{"version":3,"file":"FindModule-BzDbrX7G.js","sources":["../src/modules/FindModule.js"],"sourcesContent":["import Dialog from '../utils/Dialog.js';\n\n/**\n * Find & Replace across the editor's text content, with optional\n * regular-expression and case-sensitive matching, plus \"highlight all\".\n */\nexport default class FindModule {\n    constructor(editor) {\n        this.editor = editor;\n        this.matches = [];\n        this.currentIndex = -1;\n    }\n\n    open() {\n        this.clearHighlights();\n\n        const body = `\n            <label class=\"ife-field\">\n                <span>Find</span>\n                <input type=\"text\" name=\"query\" required autofocus>\n            </label>\n            <label class=\"ife-field\">\n                <span>Replace with</span>\n                <input type=\"text\" name=\"replacement\">\n            </label>\n            <label class=\"ife-field--inline\"><input type=\"checkbox\" name=\"caseSensitive\"> Case sensitive</label>\n            <label class=\"ife-field--inline\"><input type=\"checkbox\" name=\"useRegex\"> Regular expression</label>\n        `;\n\n        this.dialog = new Dialog(this.editor.wrapper, {\n            title: 'Find & Replace',\n            bodyHtml: body,\n            confirmLabel: 'Replace all',\n            onConfirm: (form) => this.replaceAll(form),\n            onClose: () => this.clearHighlights(),\n        });\n\n        const findNextBtn = document.createElement('button');\n        findNextBtn.type = 'button';\n        findNextBtn.className = 'ife-btn ife-btn--ghost';\n        findNextBtn.textContent = 'Highlight all';\n        findNextBtn.addEventListener('mousedown', (e) => e.preventDefault());\n        findNextBtn.addEventListener('click', (e) => {\n            e.stopPropagation();\n            this.highlightAll(new FormData(this.dialog.form));\n        });\n\n        this.dialog.open();\n        this.dialog.form.querySelector('.ife-dialog__footer').prepend(findNextBtn);\n    }\n\n    buildRegex(form) {\n        const query = String(form.get('query') ?? '').trim();\n        if (!query) return null;\n        const caseSensitive = Boolean(form.get('caseSensitive'));\n        const useRegex = Boolean(form.get('useRegex'));\n        const flags = `g${caseSensitive ? '' : 'i'}`;\n        const pattern = useRegex ? query : query.replace(/[.*+?^${}()|[\\]\\\\]/g, '\\\\$&');\n        return new RegExp(pattern, flags);\n    }\n\n    highlightAll(form) {\n        this.clearHighlights();\n        const query = String(form.get('query') ?? '');\n        if (!query) return;\n        const regex = this.buildRegex(form);\n        if (!regex) return;\n\n        const walker = document.createTreeWalker(this.editor.root, NodeFilter.SHOW_TEXT, null);\n        const textNodes = [];\n        let node = walker.nextNode();\n        while (node) {\n            textNodes.push(node);\n            node = walker.nextNode();\n        }\n\n        textNodes.forEach((textNode) => {\n            const text = textNode.textContent ?? '';\n            if (!regex.test(text)) return;\n            regex.lastIndex = 0;\n\n            const fragment = document.createDocumentFragment();\n            let lastIndex = 0;\n            let match = regex.exec(text);\n            while (match) {\n                fragment.appendChild(document.createTextNode(text.slice(lastIndex, match.index)));\n                const mark = document.createElement('mark');\n                mark.className = 'ife-search-highlight';\n                mark.textContent = match[0];\n                fragment.appendChild(mark);\n                lastIndex = match.index + match[0].length;\n                match = regex.exec(text);\n            }\n            fragment.appendChild(document.createTextNode(text.slice(lastIndex)));\n            textNode.replaceWith(fragment);\n        });\n    }\n\n    clearHighlights() {\n        this.editor.root.querySelectorAll('mark.ife-search-highlight').forEach((mark) => {\n            mark.replaceWith(document.createTextNode(mark.textContent ?? ''));\n        });\n        this.editor.root.normalize();\n    }\n\n    replaceAll(form) {\n        const data = new FormData(form);\n        const regex = this.buildRegex(data);\n        if (!regex) return;\n        const replacement = String(data.get('replacement') ?? '');\n\n        this.editor.history.push();\n        this.clearHighlights();\n\n        const walker = document.createTreeWalker(this.editor.root, NodeFilter.SHOW_TEXT, null);\n        const textNodes = [];\n        let node = walker.nextNode();\n        while (node) {\n            textNodes.push(node);\n            node = walker.nextNode();\n        }\n\n        textNodes.forEach((textNode) => {\n            textNode.textContent = (textNode.textContent ?? '').replace(regex, replacement);\n        });\n\n        this.editor.emitChange();\n    }\n\n    destroy() {\n        this.clearHighlights();\n        this.dialog?.close();\n    }\n}\n"],"names":["FindModule","editor","body","Dialog","form","findNextBtn","e","query","caseSensitive","useRegex","flags","pattern","regex","walker","textNodes","node","textNode","text","fragment","lastIndex","match","mark","data","replacement","_a"],"mappings":";AAMe,MAAMA,EAAW;AAAA,EAC5B,YAAYC,GAAQ;AAChB,SAAK,SAASA,GACd,KAAK,UAAU,CAAA,GACf,KAAK,eAAe;AAAA,EACxB;AAAA,EAEA,OAAO;AACH,SAAK,gBAAe;AAEpB,UAAMC,IAAO;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAAA;AAab,SAAK,SAAS,IAAIC,EAAO,KAAK,OAAO,SAAS;AAAA,MAC1C,OAAO;AAAA,MACP,UAAUD;AAAA,MACV,cAAc;AAAA,MACd,WAAW,CAACE,MAAS,KAAK,WAAWA,CAAI;AAAA,MACzC,SAAS,MAAM,KAAK,gBAAe;AAAA,IAC/C,CAAS;AAED,UAAMC,IAAc,SAAS,cAAc,QAAQ;AACnD,IAAAA,EAAY,OAAO,UACnBA,EAAY,YAAY,0BACxBA,EAAY,cAAc,iBAC1BA,EAAY,iBAAiB,aAAa,CAACC,MAAMA,EAAE,gBAAgB,GACnED,EAAY,iBAAiB,SAAS,CAACC,MAAM;AACzC,MAAAA,EAAE,gBAAe,GACjB,KAAK,aAAa,IAAI,SAAS,KAAK,OAAO,IAAI,CAAC;AAAA,IACpD,CAAC,GAED,KAAK,OAAO,KAAI,GAChB,KAAK,OAAO,KAAK,cAAc,qBAAqB,EAAE,QAAQD,CAAW;AAAA,EAC7E;AAAA,EAEA,WAAWD,GAAM;AACb,UAAMG,IAAQ,OAAOH,EAAK,IAAI,OAAO,KAAK,EAAE,EAAE,KAAI;AAClD,QAAI,CAACG,EAAO,QAAO;AACnB,UAAMC,IAAgB,EAAQJ,EAAK,IAAI,eAAe,GAChDK,IAAW,EAAQL,EAAK,IAAI,UAAU,GACtCM,IAAQ,IAAIF,IAAgB,KAAK,GAAG,IACpCG,IAAUF,IAAWF,IAAQA,EAAM,QAAQ,uBAAuB,MAAM;AAC9E,WAAO,IAAI,OAAOI,GAASD,CAAK;AAAA,EACpC;AAAA,EAEA,aAAaN,GAAM;AAGf,QAFA,KAAK,gBAAe,GAEhB,CADU,OAAOA,EAAK,IAAI,OAAO,KAAK,EAAE,EAChC;AACZ,UAAMQ,IAAQ,KAAK,WAAWR,CAAI;AAClC,QAAI,CAACQ,EAAO;AAEZ,UAAMC,IAAS,SAAS,iBAAiB,KAAK,OAAO,MAAM,WAAW,WAAW,IAAI,GAC/EC,IAAY,CAAA;AAClB,QAAIC,IAAOF,EAAO,SAAQ;AAC1B,WAAOE;AACH,MAAAD,EAAU,KAAKC,CAAI,GACnBA,IAAOF,EAAO,SAAQ;AAG1B,IAAAC,EAAU,QAAQ,CAACE,MAAa;AAC5B,YAAMC,IAAOD,EAAS,eAAe;AACrC,UAAI,CAACJ,EAAM,KAAKK,CAAI,EAAG;AACvB,MAAAL,EAAM,YAAY;AAElB,YAAMM,IAAW,SAAS,uBAAsB;AAChD,UAAIC,IAAY,GACZC,IAAQR,EAAM,KAAKK,CAAI;AAC3B,aAAOG,KAAO;AACV,QAAAF,EAAS,YAAY,SAAS,eAAeD,EAAK,MAAME,GAAWC,EAAM,KAAK,CAAC,CAAC;AAChF,cAAMC,IAAO,SAAS,cAAc,MAAM;AAC1C,QAAAA,EAAK,YAAY,wBACjBA,EAAK,cAAcD,EAAM,CAAC,GAC1BF,EAAS,YAAYG,CAAI,GACzBF,IAAYC,EAAM,QAAQA,EAAM,CAAC,EAAE,QACnCA,IAAQR,EAAM,KAAKK,CAAI;AAAA,MAC3B;AACA,MAAAC,EAAS,YAAY,SAAS,eAAeD,EAAK,MAAME,CAAS,CAAC,CAAC,GACnEH,EAAS,YAAYE,CAAQ;AAAA,IACjC,CAAC;AAAA,EACL;AAAA,EAEA,kBAAkB;AACd,SAAK,OAAO,KAAK,iBAAiB,2BAA2B,EAAE,QAAQ,CAACG,MAAS;AAC7E,MAAAA,EAAK,YAAY,SAAS,eAAeA,EAAK,eAAe,EAAE,CAAC;AAAA,IACpE,CAAC,GACD,KAAK,OAAO,KAAK,UAAS;AAAA,EAC9B;AAAA,EAEA,WAAWjB,GAAM;AACb,UAAMkB,IAAO,IAAI,SAASlB,CAAI,GACxBQ,IAAQ,KAAK,WAAWU,CAAI;AAClC,QAAI,CAACV,EAAO;AACZ,UAAMW,IAAc,OAAOD,EAAK,IAAI,aAAa,KAAK,EAAE;AAExD,SAAK,OAAO,QAAQ,KAAI,GACxB,KAAK,gBAAe;AAEpB,UAAMT,IAAS,SAAS,iBAAiB,KAAK,OAAO,MAAM,WAAW,WAAW,IAAI,GAC/EC,IAAY,CAAA;AAClB,QAAIC,IAAOF,EAAO,SAAQ;AAC1B,WAAOE;AACH,MAAAD,EAAU,KAAKC,CAAI,GACnBA,IAAOF,EAAO,SAAQ;AAG1B,IAAAC,EAAU,QAAQ,CAACE,MAAa;AAC5B,MAAAA,EAAS,eAAeA,EAAS,eAAe,IAAI,QAAQJ,GAAOW,CAAW;AAAA,IAClF,CAAC,GAED,KAAK,OAAO,WAAU;AAAA,EAC1B;AAAA,EAEA,UAAU;;AACN,SAAK,gBAAe,IACpBC,IAAA,KAAK,WAAL,QAAAA,EAAa;AAAA,EACjB;AACJ;"}

resources/js/dist/ImageModule-CEX5MWCb.js
@@ -1,4 +1,4 @@
-import { D as u } from "./index-Ccv0d7hY.js";
+import { D as u } from "./index-DdpIAR1K.js";
 class v {
   constructor(e) {
     this.editor = e, this.uploadUrl = e.options.uploadUrl, this.handleDrop = this.handleDrop.bind(this), this.handleClick = this.handleClick.bind(this), this.handleDblClick = this.handleDblClick.bind(this), this.handleMouseDown = this.handleMouseDown.bind(this), this.handleResizeStart = this.handleResizeStart.bind(this), e.root.addEventListener("dragover", (t) => t.preventDefault()), e.root.addEventListener("drop", this.handleDrop), e.root.addEventListener("click", this.handleClick), e.root.addEventListener("dblclick", this.handleDblClick), e.root.addEventListener("mousedown", this.handleMouseDown);
@@ -190,4 +190,4 @@ class v {
 export {
   v as default
 };
-//# sourceMappingURL=ImageModule-dGVk9NJq.js.map
+//# sourceMappingURL=ImageModule-CEX5MWCb.js.map

resources/js/dist/LinkModule-BviLFBxw.js.map
@@ -1,1 +1,0 @@
-{"version":3,"file":"LinkModule-BviLFBxw.js","sources":["../src/modules/LinkModule.js"],"sourcesContent":["import Dialog from '../utils/Dialog.js';\n\n/**\n * Full link management: insert, edit (url, text, title, target, rel flags),\n * and remove — matching the \"полная поддержка ссылок\" requirement.\n */\nexport default class LinkModule {\n    constructor(editor) {\n        this.editor = editor;\n        this.handleDblClick = this.handleDblClick.bind(this);\n        // Lets people jump straight into editing a link by double-clicking it,\n        // instead of having to place the caret inside it and hunt for the\n        // toolbar button.\n        editor.root.addEventListener('dblclick', this.handleDblClick);\n    }\n\n    /** @param {MouseEvent} event */\n    handleDblClick(event) {\n        const anchor = event.target.closest?.('a');\n        if (!anchor || !this.editor.root.contains(anchor)) return;\n        event.preventDefault();\n\n        const range = document.createRange();\n        range.selectNodeContents(anchor);\n        this.editor.selection.setRange(range);\n        this.editor.selection.save();\n\n        this.open();\n    }\n\n    open() {\n        const existing = this.editor.selection.closest('a');\n        const selectedText = this.editor.selection.getText();\n\n        const body = `\n            <label class=\"ife-field\">\n                <span>Text</span>\n                <input type=\"text\" name=\"text\" value=\"${this.escape(existing?.textContent ?? selectedText)}\" required>\n            </label>\n            <label class=\"ife-field\">\n                <span>URL</span>\n                <input type=\"url\" name=\"href\" value=\"${this.escape(existing?.getAttribute('href') ?? 'https://')}\" required>\n            </label>\n            <label class=\"ife-field\">\n                <span>Title</span>\n                <input type=\"text\" name=\"title\" value=\"${this.escape(existing?.getAttribute('title') ?? '')}\">\n            </label>\n            <label class=\"ife-field ife-field--inline\">\n                <input type=\"checkbox\" name=\"newTab\" ${existing?.target === '_blank' ? 'checked' : ''}>\n                <span>Open in new tab</span>\n            </label>\n            <fieldset class=\"ife-field-group\">\n                <legend>rel</legend>\n                <label class=\"ife-field--inline\"><input type=\"checkbox\" name=\"nofollow\" ${existing?.rel.includes('nofollow') ? 'checked' : ''}> nofollow</label>\n                <label class=\"ife-field--inline\"><input type=\"checkbox\" name=\"noopener\" ${existing?.rel.includes('noopener') ? 'checked' : ''}> noopener</label>\n                <label class=\"ife-field--inline\"><input type=\"checkbox\" name=\"noreferrer\" ${existing?.rel.includes('noreferrer') ? 'checked' : ''}> noreferrer</label>\n            </fieldset>\n        `;\n\n        this.dialog = new Dialog(this.editor.wrapper, {\n            title: existing ? 'Edit link' : 'Insert link',\n            bodyHtml: body,\n            confirmLabel: existing ? 'Update' : 'Insert',\n            onConfirm: (form) => this.apply(form, existing),\n        });\n\n        this.editor.selection.save();\n        this.dialog.open();\n\n        if (existing) {\n            const removeBtn = document.createElement('button');\n            removeBtn.type = 'button';\n            removeBtn.className = 'ife-btn ife-btn--danger';\n            removeBtn.textContent = 'Remove link';\n            removeBtn.addEventListener('mousedown', (e) => e.preventDefault());\n            removeBtn.addEventListener('click', (e) => {\n                e.stopPropagation();\n                this.remove(existing);\n                this.dialog.close();\n            });\n            this.dialog.form.querySelector('.ife-dialog__footer').prepend(removeBtn);\n        }\n    }\n\n    apply(form, existing) {\n        const data = new FormData(form);\n        const rel = ['nofollow', 'noopener', 'noreferrer'].filter((flag) => data.get(flag)).join(' ');\n\n        const anchor = existing ?? document.createElement('a');\n        anchor.textContent = String(data.get('text'));\n\n        // Fallback to '#' for unsafe URLs (javascript:, etc.) so the link\n        // is still created but harmless, rather than silently dropping the\n        // attribute which would default to the current page URL.\n        const href = String(data.get('href'));\n        anchor.setAttribute('href', this.editor.sanitizer.isSafeUrl(href) ? href : '#');\n        anchor.setAttribute('title', String(data.get('title') ?? ''));\n        anchor.setAttribute('target', data.get('newTab') ? '_blank' : '_self');\n        if (rel) anchor.setAttribute('rel', rel);\n        else anchor.removeAttribute('rel');\n\n        this.editor.history.push();\n\n        if (!existing) {\n            this.editor.selection.restore();\n            const range = this.editor.selection.getRange();\n            range?.deleteContents();\n            range?.insertNode(anchor);\n        }\n\n        this.editor.emitChange();\n    }\n\n    remove(anchor) {\n        this.editor.history.push();\n        const parent = anchor.parentNode;\n        while (anchor.firstChild) parent.insertBefore(anchor.firstChild, anchor);\n        parent.removeChild(anchor);\n        this.editor.emitChange();\n    }\n\n    escape(value) {\n        return String(value ?? '')\n            .replace(/&/g, '&amp;')\n            .replace(/\"/g, '&quot;')\n            .replace(/</g, '&lt;')\n            .replace(/>/g, '&gt;');\n    }\n\n    destroy() {\n        this.dialog?.close();\n        this.editor.root.removeEventListener('dblclick', this.handleDblClick);\n    }\n}\n"],"names":["LinkModule","editor","event","anchor","_b","_a","range","existing","selectedText","body","Dialog","form","removeBtn","e","data","rel","flag","href","parent","value"],"mappings":";AAMe,MAAMA,EAAW;AAAA,EAC5B,YAAYC,GAAQ;AAChB,SAAK,SAASA,GACd,KAAK,iBAAiB,KAAK,eAAe,KAAK,IAAI,GAInDA,EAAO,KAAK,iBAAiB,YAAY,KAAK,cAAc;AAAA,EAChE;AAAA;AAAA,EAGA,eAAeC,GAAO;;AAClB,UAAMC,KAASC,KAAAC,IAAAH,EAAM,QAAO,YAAb,gBAAAE,EAAA,KAAAC,GAAuB;AACtC,QAAI,CAACF,KAAU,CAAC,KAAK,OAAO,KAAK,SAASA,CAAM,EAAG;AACnD,IAAAD,EAAM,eAAc;AAEpB,UAAMI,IAAQ,SAAS,YAAW;AAClC,IAAAA,EAAM,mBAAmBH,CAAM,GAC/B,KAAK,OAAO,UAAU,SAASG,CAAK,GACpC,KAAK,OAAO,UAAU,KAAI,GAE1B,KAAK,KAAI;AAAA,EACb;AAAA,EAEA,OAAO;AACH,UAAMC,IAAW,KAAK,OAAO,UAAU,QAAQ,GAAG,GAC5CC,IAAe,KAAK,OAAO,UAAU,QAAO,GAE5CC,IAAO;AAAA;AAAA;AAAA,wDAGmC,KAAK,QAAOF,KAAA,gBAAAA,EAAU,gBAAeC,CAAY,CAAC;AAAA;AAAA;AAAA;AAAA,uDAInD,KAAK,QAAOD,KAAA,gBAAAA,EAAU,aAAa,YAAW,UAAU,CAAC;AAAA;AAAA;AAAA;AAAA,yDAIvD,KAAK,QAAOA,KAAA,gBAAAA,EAAU,aAAa,aAAY,EAAE,CAAC;AAAA;AAAA;AAAA,wDAGpDA,KAAA,gBAAAA,EAAU,YAAW,WAAW,YAAY,EAAE;AAAA;AAAA;AAAA;AAAA;AAAA,0FAKXA,KAAA,QAAAA,EAAU,IAAI,SAAS,cAAc,YAAY,EAAE;AAAA,0FACnDA,KAAA,QAAAA,EAAU,IAAI,SAAS,cAAc,YAAY,EAAE;AAAA,4FACjDA,KAAA,QAAAA,EAAU,IAAI,SAAS,gBAAgB,YAAY,EAAE;AAAA;AAAA;AAczI,QAVA,KAAK,SAAS,IAAIG,EAAO,KAAK,OAAO,SAAS;AAAA,MAC1C,OAAOH,IAAW,cAAc;AAAA,MAChC,UAAUE;AAAA,MACV,cAAcF,IAAW,WAAW;AAAA,MACpC,WAAW,CAACI,MAAS,KAAK,MAAMA,GAAMJ,CAAQ;AAAA,IAC1D,CAAS,GAED,KAAK,OAAO,UAAU,KAAI,GAC1B,KAAK,OAAO,KAAI,GAEZA,GAAU;AACV,YAAMK,IAAY,SAAS,cAAc,QAAQ;AACjD,MAAAA,EAAU,OAAO,UACjBA,EAAU,YAAY,2BACtBA,EAAU,cAAc,eACxBA,EAAU,iBAAiB,aAAa,CAACC,MAAMA,EAAE,gBAAgB,GACjED,EAAU,iBAAiB,SAAS,CAACC,MAAM;AACvC,QAAAA,EAAE,gBAAe,GACjB,KAAK,OAAON,CAAQ,GACpB,KAAK,OAAO,MAAK;AAAA,MACrB,CAAC,GACD,KAAK,OAAO,KAAK,cAAc,qBAAqB,EAAE,QAAQK,CAAS;AAAA,IAC3E;AAAA,EACJ;AAAA,EAEA,MAAMD,GAAMJ,GAAU;AAClB,UAAMO,IAAO,IAAI,SAASH,CAAI,GACxBI,IAAM,CAAC,YAAY,YAAY,YAAY,EAAE,OAAO,CAACC,MAASF,EAAK,IAAIE,CAAI,CAAC,EAAE,KAAK,GAAG,GAEtFb,IAASI,KAAY,SAAS,cAAc,GAAG;AACrD,IAAAJ,EAAO,cAAc,OAAOW,EAAK,IAAI,MAAM,CAAC;AAK5C,UAAMG,IAAO,OAAOH,EAAK,IAAI,MAAM,CAAC;AASpC,QARAX,EAAO,aAAa,QAAQ,KAAK,OAAO,UAAU,UAAUc,CAAI,IAAIA,IAAO,GAAG,GAC9Ed,EAAO,aAAa,SAAS,OAAOW,EAAK,IAAI,OAAO,KAAK,EAAE,CAAC,GAC5DX,EAAO,aAAa,UAAUW,EAAK,IAAI,QAAQ,IAAI,WAAW,OAAO,GACjEC,IAAKZ,EAAO,aAAa,OAAOY,CAAG,IAClCZ,EAAO,gBAAgB,KAAK,GAEjC,KAAK,OAAO,QAAQ,KAAI,GAEpB,CAACI,GAAU;AACX,WAAK,OAAO,UAAU,QAAO;AAC7B,YAAMD,IAAQ,KAAK,OAAO,UAAU,SAAQ;AAC5C,MAAAA,KAAA,QAAAA,EAAO,kBACPA,KAAA,QAAAA,EAAO,WAAWH;AAAA,IACtB;AAEA,SAAK,OAAO,WAAU;AAAA,EAC1B;AAAA,EAEA,OAAOA,GAAQ;AACX,SAAK,OAAO,QAAQ,KAAI;AACxB,UAAMe,IAASf,EAAO;AACtB,WAAOA,EAAO,aAAY,CAAAe,EAAO,aAAaf,EAAO,YAAYA,CAAM;AACvE,IAAAe,EAAO,YAAYf,CAAM,GACzB,KAAK,OAAO,WAAU;AAAA,EAC1B;AAAA,EAEA,OAAOgB,GAAO;AACV,WAAO,OAAOA,KAAS,EAAE,EACpB,QAAQ,MAAM,OAAO,EACrB,QAAQ,MAAM,QAAQ,EACtB,QAAQ,MAAM,MAAM,EACpB,QAAQ,MAAM,MAAM;AAAA,EAC7B;AAAA,EAEA,UAAU;;AACN,KAAAd,IAAA,KAAK,WAAL,QAAAA,EAAa,SACb,KAAK,OAAO,KAAK,oBAAoB,YAAY,KAAK,cAAc;AAAA,EACxE;AACJ;"}

commit d3189a705d464f9cb5080029ae608bc11a8a6494

fix: escape <x-editor> initial value in the Blade textarea
Restores HTML-escaping of the value written into the hidden textarea
(e(, doubleEncode:false)): the PHP feature test asserts the render
contains &lt;p&gt;hi&lt;/p&gt; for :value="'<p>hi</p>'". The raw output
({!! !!}) committed in 4361c51 broke that contract. double_encode=false
keeps already-stored entities (e.g. &amp;) from being re-encoded, and
escaped < > also neutralise </textarea> breakout, so the preg_replace
guard is no longer needed.

resources/views/components/editor.blade.php
@@ -46,7 +46,7 @@
         name="{{ $name }}"
         id="{{ $elementId }}"
         style="display:none"
-    >{!! preg_replace('/<\/textarea>/i', '&lt;/textarea&gt;', $value) !!}</textarea>
+    >{{ e($value, false) }}</textarea>
 </div>
 
 @once